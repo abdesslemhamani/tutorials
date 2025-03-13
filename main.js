045dd624670900ee658fa85c3176c88b3ec8fd66
abdesslemhamani <abdesslemhamani@gmail.com>
2025-03-13 03:12:01 +0100

    final test

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-let my =[ "keyboard" , "Mouse" , "Pen","Pad","Monitor","iphone" ];
+let products =[ "keyboard" , "Mouse" , "Pen","Pad","Monitor","iphone" ];
 let colors = ["red","green","bleu"];
-let showCount =5;
+let showCount =6;
 
 document.write(`<h1>show ${showCount} products</h1>`)
+for(let i=0;i<showCount ;i++){
+
+document.write(`<div>`);
+document.write(`<h3>${i+1}${"-"}${products[i]}</h3>`)
+
+for(let j = 0 ; j<colors.length;j++){
+    // document.write(`<p>${colors[j]}</p>`);
+}
+document.write(`<p>${colors.join(" | ")}</p>`);
+document.write(`</div>`);
+}
